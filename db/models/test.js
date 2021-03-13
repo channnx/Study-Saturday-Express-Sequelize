45e448f459df102ba9ebddeca0cc247036f5fb25
Shiyang Chen <ana.chensy@gmail.com>
2021-03-13 11:37:39 -0500

    finished c2

--- db/models/test.js
+++ db/models/test.js
@@ -2,6 +2,15 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
-
+const Test = db.define('text', {
+    subject : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    grade: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    }
+});
+Test.belongsTo(Student)
 module.exports = Test;
